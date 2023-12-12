85acfde829bf60859eea66a4287e52a12c84b063
thijnmens <thijnsmulders04@gmail.com>
2023-12-12 17:25:22 +0100

    Fix saveFile path for non-windows systems

--- src/main/java/com/adosar/backend/business/impl/UserManagerImpl.java
+++ src/main/java/com/adosar/backend/business/impl/UserManagerImpl.java
@@ -23,6 +23,7 @@ import com.password4j.BadParametersException;
 import com.password4j.Hash;
 import com.password4j.Password;
 import lombok.AllArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
--- src/main/java/com/adosar/backend/business/service/CdnService.java
+++ src/main/java/com/adosar/backend/business/service/CdnService.java
@@ -1,5 +1,7 @@
 package com.adosar.backend.business.service;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,9 +11,10 @@ public final class CdnService {
 	private static final String CDN_PATH = System.getenv("CDN_PATH");
 	
 	public static void saveFile(byte[] data, String path, String filename) throws IOException {
-		File file = new File(String.format("%s%s\\%s", CDN_PATH, path, filename));
-		OutputStream outputStream = new FileOutputStream(file);
-		outputStream.write(data);
-		outputStream.close();
+		String fullPath = FilenameUtils.separatorsToSystem(String.format("%s%s\\%s", CDN_PATH, path, filename));
+		File file = new File(fullPath);
+		try (OutputStream outputStream = new FileOutputStream(file)) {
+			outputStream.write(data);
+		}
 	}
 }
